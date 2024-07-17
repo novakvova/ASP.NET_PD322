48b0dbae436c17c6f7b9f07cc6a6d38f69f4f9a4
novakvova <novakvova@gmail.com>
2024-07-17 16:05:07 +0300

    View grid

--- WebHalk/WebHalk/Controllers/MainController.cs
+++ WebHalk/WebHalk/Controllers/MainController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebHalk.Data;
+using WebHalk.Models.Categories;
 
 namespace WebHalk.Controllers
 {
     public class MainController : Controller
     {
+        private readonly HulkDbContext _hulkDbContext;
+
+        public MainController(HulkDbContext hulkDbContext)
+        {
+            _hulkDbContext = hulkDbContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            var list = _hulkDbContext.Categories
+                .Select(x=> new CategoryItemViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Image = x.Image
+                })
+                .ToList();
+            return View(list);
         }
     }
 }
---
+++ WebHalk/WebHalk/Models/Categories/CategoryItemViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebHalk.Models.Categories
+{
+    public class CategoryItemViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+    }
+}
