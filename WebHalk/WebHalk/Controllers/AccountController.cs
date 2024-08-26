20cdf1a7231d8dda7df9564dd69adba08a42b4bb
novakvova <novakvova@gmail.com>
2024-08-26 16:52:52 +0300

    Register user is good

---
+++ WebHalk/WebHalk/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebHalk.Constants;
+using WebHalk.Data.Entities.Identity;
+using WebHalk.Models.Account;
+
+namespace WebHalk.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<UserEntity> _userManager;
+
+        public AccountController(UserManager<UserEntity> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Code to create the user and save to the database
+                // ...
+                var user = new UserEntity
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    UserName = model.Email,
+                    Email = model.Email
+                };
+
+                var outcome = await _userManager.CreateAsync(user, model.Password);
+                if (outcome != null)
+                {
+                    if (!outcome.Succeeded)
+                    {
+                        outcome = await _userManager.AddToRoleAsync(user, Roles.User);
+                    }
+                }
+
+                return RedirectToAction("Index", "Main");
+            }
+
+            // If model state is not valid, return the view with the same model
+            return View(model);
+        }
+    }
+}
---
+++ WebHalk/WebHalk/Models/Account/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebHalk.Models.Account
+{
+    public class RegisterViewModel
+    {
+        [Display(Name = "Ім'я")]
+        [Required(ErrorMessage = "Вкажіть ім'я")]
+        public string FirstName { get; set; } = null!;
+        [Display(Name = "Прізвище")]
+        [Required(ErrorMessage = "Вкажіть ім'я")]
+        public string LastName { get; set; } = null!;
+        [Display(Name = "Електронна пошта")]
+        [Required(ErrorMessage = "Вкажіть назву пошти")]
+        [EmailAddress(ErrorMessage = "Пошту вказано не коректно")]
+        public string Email { get; set; } = null!;
+        [Display(Name = "Пароль")]
+        [Required(ErrorMessage ="Вкажіть пароль")]
+        [MinLength(6, ErrorMessage = "Довжина паролю має бути мін 6 символів")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+        [Display(Name = "Підтвердежння пароля")]
+        [Required(ErrorMessage ="Вкажіть пароль повторно")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Паролі не співпадають")]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
