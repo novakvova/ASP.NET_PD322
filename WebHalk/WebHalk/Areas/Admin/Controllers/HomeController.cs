ee66274128ae2faa51e4a4a809f78e727fa3a813
novakvova <novakvova@gmail.com>
2024-09-02 17:52:47 +0300

    Test role Admin

--- WebHalk/WebHalk/Areas/Admin/Controllers/HomeController.cs
+++ WebHalk/WebHalk/Areas/Admin/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebHalk.Constants;
 
 namespace WebHalk.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = Roles.Admin)]
     public class HomeController : Controller
     {
         public IActionResult Index()
