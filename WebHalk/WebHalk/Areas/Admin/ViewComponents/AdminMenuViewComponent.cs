f58f1fbeaafaad4e08f07bf814aeb5373cc9369d
novakvova <novakvova@gmail.com>
2024-09-02 16:21:43 +0300

    Fix component

---
+++ WebHalk/WebHalk/Areas/Admin/ViewComponents/AdminMenuViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebHalk.Areas.Admin.Models.Profile;
+using WebHalk.Data.Entities.Identity;
+
+namespace WebHalk.Areas.Admin.ViewComponents
+{
+    public class AdminMenuViewComponent : ViewComponent
+    {
+        private UserManager<UserEntity> _userManager;
+        public AdminMenuViewComponent(UserManager<UserEntity> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            ProfileItemViewModel model = new ProfileItemViewModel();
+            model.FirstName = user.FirstName;
+            model.LastName = user.LastName;
+
+            return View(model);
+        }
+    }
+}
