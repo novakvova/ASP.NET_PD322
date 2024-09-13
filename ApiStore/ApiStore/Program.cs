70f1f1f687cc3bb0f4a8f731d26808525a323a87
novakvova <novakvova@gmail.com>
2024-09-13 15:56:05 +0300

    Add Migration is good

--- ApiStore/ApiStore/Program.cs
+++ ApiStore/ApiStore/Program.cs
@@ -1,6 +1,12 @@
+using ApiStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApiStoreDbContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
