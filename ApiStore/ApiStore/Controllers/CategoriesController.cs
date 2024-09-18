b9062a83e352b2d035e4b11c8608bef524f56d57
novakvova <novakvova@gmail.com>
2024-09-18 16:10:16 +0300

    Add Webp format

--- ApiStore/ApiStore/Controllers/CategoriesController.cs
+++ ApiStore/ApiStore/Controllers/CategoriesController.cs
@@ -1,19 +1,56 @@
 ﻿using ApiStore.Data;
+using ApiStore.Models.Category;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Formats.Webp;
+using SixLabors.ImageSharp.Processing;
 
 namespace ApiStore.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class CategoriesController(
-        ApiStoreDbContext context) : ControllerBase
+        ApiStoreDbContext context, IConfiguration configuration) : ControllerBase
     {
         [HttpGet]
         public IActionResult GetList()
         {
             var list = context.Categories.ToList();
             return Ok(list);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromForm]CategoryCreateViewModel model)
+        {
+            string imageName = Guid.NewGuid().ToString()+".webp";
+            var dir = configuration["ImagesDir"];
+            
+            using (MemoryStream ms = new())
+            {
+                await model.Image.CopyToAsync(ms);
+                var bytes = ms.ToArray();
+                int[] sizes = [50, 150, 300, 600, 1200];
+                foreach (var size in sizes)
+                {
+                    string dirSave = Path.Combine(Directory.GetCurrentDirectory(),
+                        dir, $"{size}_{imageName}");
+                    using (var image = Image.Load(bytes))
+                    {
+                        // Resize the image (50% of original dimensions)
+                        image.Mutate(x => x.Resize(new ResizeOptions
+                        {
+                            Size = new Size(size, size),
+                            Mode = ResizeMode.Max
+                        }));
+
+                        // Save the image with compression
+                        image.Save(dirSave,  new WebpEncoder());
+                    }
+                }
+            }
+            return Ok();
         }
     }
 }
---
+++ ApiStore/ApiStore/Models/Category/CategoryCreateViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiStore.Models.Category
+{
+    public class CategoryCreateViewModel
+    {
+        public string Name { get; set; } = String.Empty;
+        public IFormFile? Image { get; set; }
+        public string? Description { get; set; }
+    }
+}
