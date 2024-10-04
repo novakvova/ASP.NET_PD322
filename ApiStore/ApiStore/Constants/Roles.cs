9571c356b28a1cfeb10d7c67a9a895d9db369f02
novakvova <novakvova@gmail.com>
2024-10-04 16:22:24 +0300

    Seed Data User

---
+++ ApiStore/ApiStore/Constants/Roles.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiStore.Constants
+{
+    public static class Roles
+    {
+        public const string Admin = "admin";
+        public const string User = "user";
+    }
+}
--- ApiStore/ApiStore/Program.cs
+++ ApiStore/ApiStore/Program.cs
@@ -1,9 +1,12 @@
+using ApiStore.Constants;
 using ApiStore.Data;
 using ApiStore.Data.Entities;
+using ApiStore.Data.Entities.Identity;
 using ApiStore.Interfaces;
 using ApiStore.Mapper;
 using ApiStore.Services;
 using Bogus;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
 using System;
@@ -13,6 +16,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<ApiStoreDbContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<UserEntity, RoleEntity>(options =>
+{
+    options.Stores.MaxLengthForKeys = 128;
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 5;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+})
+    .AddEntityFrameworkStores<ApiStoreDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddAutoMapper(typeof(AppMapProfile));
 builder.Services.AddScoped<IImageHulk, ImageHulk>();
@@ -58,6 +73,8 @@ using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<ApiStoreDbContext>();
     var imageHulk = scope.ServiceProvider.GetRequiredService<IImageHulk>();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<UserEntity>>();
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<RoleEntity>>();
     //dbContext.Database.EnsureDeleted();
     dbContext.Database.Migrate();
 
@@ -111,6 +128,43 @@ using (var scope = app.Services.CreateScope())
                 dbContext.Add(imageProduct);
                 dbContext.SaveChanges();
             }
+        }
+    }
+
+    // seed roles
+    if (dbContext.Roles.Count() == 0)
+    {
+        var roles = new[]
+        {
+            new RoleEntity { Name = Roles.Admin },
+            new RoleEntity { Name = Roles.User }
+        };
+
+        foreach (var role in roles)
+        {
+            var outcome = roleManager.CreateAsync(role).Result;
+            if (!outcome.Succeeded) Console.WriteLine($"Failed to create role: {role.Name}");
+        }
+    }
+
+    // seed users
+    if (dbContext.Users.Count() == 0)
+    {
+        var users = new[]
+        {
+            new { User = new UserEntity { Firstname = "Tony", Lastname = "Stark", UserName = "admin@gmail.com", Email = "admin@gmail.com" }, Password = "admin1", Role = Roles.Admin },
+            new { User = new UserEntity { Firstname = "Boba", Lastname = "Gray", UserName = "user@gmail.com", Email = "user@gmail.com" }, Password = "bobapass1", Role = Roles.User },
+            new { User = new UserEntity { Firstname = "Biba", Lastname = "Undefined", UserName = "biba@gmail.com", Email = "biba@gmail.com" }, Password = "bibapass3", Role = Roles.User }
+        };
+
+        foreach (var i in users)
+        {
+            var outcome = userManager.CreateAsync(i.User, i.Password).Result;
+
+            if (!outcome.Succeeded)
+                Console.WriteLine($"Failed to create user: {i.User.UserName}");
+            else
+                outcome = userManager.AddToRoleAsync(i.User, i.Role).Result;
         }
     }
 }
